c8a368be47026d3f64f9007d9d6701428f5f268b
Nitesh <niteshmali132002@gmail.com>
2023-02-25 18:26:37 +0530

    Added more functionalities

--- web_app.py
+++ web_app.py
@@ -21,6 +21,7 @@ Categories : Holly/Bolly - Genre - Actors - Series
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
+import requests
 from flask import Flask, request, render_template
 
 
@@ -39,12 +40,56 @@ def combFeatures(row):
 def getTitle(index):
     return df[df.index == index]["title"].values[0]
 
+#Returns cast from the index of the movie
+def getCast(index):
+    return df[df.index == index]["cast"].values[0]
+
+#Returns genre from the index of the movie
+def getGenre(index):
+    return df[df.index == index]["genres"].values[0]
+
+#Returns genre from the index of the movie
+def getDirector(index):
+    return df[df.index == index]["director"].values[0]
+
+#Returns genre from the index of the movie
+def getDate(index):
+    return df[df.index == index]["release_date"].values[0]
+
+#Returns genre from the index of the movie
+def getTagline(index):
+    return df[df.index == index]["tagline"].values[0]
+
 #Returns index from the title of the movie and if not found returns -1
 def getIndex(title):
     if(str((df[df.title == title])).startswith("Empty")):
         return -1
     return df[df.title == title]["index"].values[0]
 
+#Returns movie poster url from the title of the movie and not found returns placeholder
+def getPostInfo(index):
+    defaultObj = {'poster_url': 'https://via.placeholder.com/130x207', 'overview': getTagline(index)}
+
+    endpoint = 'https://api.themoviedb.org/3/search/movie'
+    api_key = '15d2ea6d0dc1d476efbca3eba2b9bbfb'
+    params = {'api_key': api_key, 'query': ':'.join(getTitle(index).split(':')[:2]).strip()}
+
+    response = requests.get(endpoint, params=params)
+    data = response.json()
+    results = data.get('results', [])
+    if results:
+        first_result = results[0]
+        poster_path = first_result.get('poster_path')
+        if poster_path:
+            poster_url = f'https://image.tmdb.org/t/p/original{poster_path}'
+        else:
+            poster_path = 'https://via.placeholder.com/130x207'
+        overview = first_result.get('overview')
+        overview = overview.split('.')[0].strip() + '.'
+        return {'poster_url': poster_url, 'overview': overview}
+
+    # If no poster URL is found, return placeholder
+    return defaultObj
 
 
 
@@ -94,7 +139,16 @@ def recommend_movies():
     recommended_movies = []
     i = 0
     for element in sorted_similar:
-        recommended_movies.append(getTitle(element[0]))
+        movieObj = {}
+        postandInfo = getPostInfo(element[0])
+        movieObj["title"] = getTitle(element[0])
+        movieObj["cast"] = getCast(element[0])
+        movieObj["genre"] = getGenre(element[0])
+        movieObj["director"] = getDirector(element[0])
+        movieObj["release_date"] = getDate(element[0])
+        movieObj["poster"] = postandInfo.get('poster_url')
+        movieObj["description"] = postandInfo.get('overview')
+        recommended_movies.append(movieObj)
         i += 1
         if i == movie_ret_count:
             break
