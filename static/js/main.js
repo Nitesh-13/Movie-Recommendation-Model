c6c9bd3f14b67494e61b598cfecac149f38ee9c4
Nitesh <niteshmali132002@gmail.com>
2023-03-09 13:38:05 +0530

    Added Categories for fetching movies

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,12 @@ const swiper = new Swiper(".swiper", {
 });
 
 const gotoSlide = (index) => {
+
+    const input = document.getElementById("category");
+    console.log(input)
+    const heading = document.getElementById("putCategory");
+    heading.textContent = "Enter "+ input.value.split(" ").shift() + " name you want.";
+    console.log(heading.textContent)
     swiper.slideTo(index);
 };
 
@@ -25,3 +31,20 @@ const restart = () => {
 const checkValid = (event) => {
     event.target.nextElementSibling.disabled = !event.target.value.length;
 };
+
+// get elements
+const ddMenu = document.querySelector('.dd-menu');
+const ddButton = document.querySelector('.dd-button');
+const hiddenInput = document.querySelector('#category');
+
+// add event listener to dropdown menu
+ddMenu.addEventListener('click', function(event) {
+  // get the selected item
+  const selectedItem = event.target;
+
+  // set the value of the hidden input field
+  hiddenInput.value = selectedItem.textContent;
+
+  // update the text of the dd-button div
+  ddButton.textContent = selectedItem.textContent;
+});
