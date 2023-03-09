573a4a7f54b8798c59b64aec058437b32ec19997
Nitesh <niteshmali132002@gmail.com>
2023-03-09 13:40:13 +0530

    removed unnecessary script

--- static/js/main.js
+++ static/js/main.js
@@ -13,21 +13,6 @@ const gotoSlide = (index) => {
     swiper.slideTo(index);
 };
 
-const restart = () => {
-    const inputs = document.querySelectorAll("input");
-    const buttons = document.querySelectorAll("button[type=button]");
-
-    buttons.forEach((button) => {
-        button.disabled = true;
-    });
-
-    inputs.forEach((input) => {
-        input.value = "";
-    });
-
-    gotoSlide(0);
-};
-
 const checkValid = (event) => {
     event.target.nextElementSibling.disabled = !event.target.value.length;
 };
